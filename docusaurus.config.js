fe482c8df58b47d6d177c1605af82856a4c611a6
Bima Mahendra <98264074+BimaBizz@users.noreply.github.com>
2024-08-17 04:25:03 +0700

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -44,7 +44,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/BimaBizz/dock-cockpitnest/',
+            'https://github.com/BimaBizz/dock-cockpitnest/tree/main/',
         },
         // blog: {
         //   showReadingTime: true,
